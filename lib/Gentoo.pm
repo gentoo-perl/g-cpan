9f0f5a78d15947fcda1ebcf14fb6f4f6e6f02d93
Michael Cummings <mcummings@gentoo.org>
2006-05-09 03:29:52 +0000

    Nightly commit

--- lib/Gentoo.pm
+++ lib/Gentoo.pm
@@ -4,30 +4,185 @@ use 5.008007;
 use strict;
 use warnings;
 
+# These libraries were influenced and largely written by
+# Christian Harmann <ian@gentoo.org> originally. All of the good
+# parts are ian's - the rest is mcummings messing around.
+
 require Exporter;
-use AutoLoader qw(AUTOLOAD);
 
 our @ISA = qw(Exporter);
 
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Gentoo ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
+our @EXPORT = qw( getAvailableEbuilds 
+	getCPANPackages	
 );
 
 our $VERSION = '0.01';
 
+sub new {
+     my $self  = shift;
+     my $class = ref($self) || $self;
+     return bless {}, $class;
+}
+
+sub getPerlPackages
+{
+	my %excludeDirs			= ("." => 1, ".." => 1, "metadata" => 1, "licenses" => 1, "eclass" => 1, "distfiles" => 1, "virtual" => 1, "profiles" => 1);
+	my @matches			= ();
+	my $dhp;
+	my $tc;
+	my $tp;
+	
+	foreach $tc (@scan_portage_categories)
+	{
+		$dhp = new DirHandle($portdir."/".$tc);
+		while (defined($tp = $dhp->read))
+		{
+			# - not excluded and $_ is a dir?
+			if (! $excludeDirs{$tp} && -d $portdir."/".$tc."/".$tp)
+			{
+				@tmp_availableVersions=();
+				my @tmp_availableEbuilds = getAvailableEbuilds($portdir,$tc."/".$tp);
+				foreach (@tmp_availableEbuilds)
+				{
+					push(@tmp_availableVersions,getEbuildVersionSpecial($_));
+				}
+				
+				# - get highest version >
+				if ($#tmp_availableVersions>-1)
+				{
+					$modules{'portage_lc_realversion'}{lc($tp)}=(sort(@tmp_availableVersions))[$#tmp_availableVersions];
+					$modules{'portage_lc'}{lc($tp)}=$modules{'portage_lc_realversion'}{lc($tp)};
+					
+					# - get rid of -rX >
+					$modules{'portage_lc'}{lc($tp)}=~s/([a-zA-Z0-9\-_\/]+)-r[0-9+]/$1/;
+					$modules{'portage_lc'}{lc($tp)}=~s/([a-zA-Z0-9\-_\/]+)-rc[0-9+]/$1/;
+					$modules{'portage_lc'}{lc($tp)}=~s/([a-zA-Z0-9\-_\/]+)_p[0-9+]/$1/;
+					$modules{'portage_lc'}{lc($tp)}=~s/([a-zA-Z0-9\-_\/]+)_pre[0-9+]/$1/;
+					
+					# - get rid of other stuff we don't want >
+					$modules{'portage_lc'}{lc($tp)}=~s/([a-zA-Z0-9\-_\/]+)_alpha[0-9+]?/$1/;
+					$modules{'portage_lc'}{lc($tp)}=~s/([a-zA-Z0-9\-_\/]+)_beta[0-9+]?/$1/;
+					$modules{'portage_lc'}{lc($tp)}=~s/[a-zA-Z]+$//;
+
+					$modules{'portage'}{lc($tp)}{'name'}=$tp;
+					$modules{'portage'}{lc($tp)}{'category'}=$tc;
+				}
+			}
+		}
+		undef $dhp;
+	}
+}
+
+# Description:
+# @listOfEbuilds = getAvailableEbuilds($PORTDIR, category/packagename);
+sub getAvailableEbuilds {
+	my $self = shift;
+    my $PORTDIR     = shift;
+    my $catPackage  = shift;
+    my @packagelist = ();
+
+    if ( -e $PORTDIR . "/" . $catPackage ) {
+
+        # - get list of ebuilds >
+        my $dh = new DirHandle( $PORTDIR . "/" . $catPackage );
+        while ( defined( $_ = $dh->read ) ) {
+            if ( $_ =~ m/(.+)\.ebuild$/ ) {
+                push( @packagelist, $_ );
+            }
+        }
+    }
+
+    return @packagelist;
+}
+
+# Description:
+# Returns version of an ebuild. (Without -rX string etc.)
+# $version = getEbuildVersionSpecial("foo-1.23-r1.ebuild");
+sub getEbuildVersionSpecial {
+    my $ebuildVersion = shift;
+    $ebuildVersion = substr( $ebuildVersion, 0, length($ebuildVersion) - 7 );
+    $ebuildVersion =~
+s/^([a-zA-Z0-9\-_\/\+]*)-([0-9\.]+[a-zA-Z]?)([\-r|\-rc|_alpha|_beta|_pre|_p]?)/$2$3/;
+
+    return $ebuildVersion;
+}
+
+sub getCPANPackages {
+    my $force_cpan_reload = shift;
+    my $cpan_pn           = "";
+    my @tmp_v             = ();
+
+    if ($force_cpan_reload) {
+
+        # - User forced reload of the CPAN index >
+        CPAN::Index->force_reload();
+    }
+
+    for my $mod ( CPAN::Shell->expand( "Module", "/./" ) ) {
+        if ( defined $mod->cpan_version ) {
+
+            # - Fetch CPAN-filename and cut out the filename of the tarball.
+            #   We are not using $mod->id here because doing so would end up
+            #   missing a lot of our ebuilds/packages >
+            $cpan_pn = $mod->cpan_file;
+            $cpan_pn =~ s|.*/||;
+
+            if ( $mod->cpan_version eq "undef"
+                && ( $cpan_pn =~ m/ / || $cpan_pn eq "" || !$cpan_pn ) )
+            {
+
+                # - invalid line - skip that one >
+                next;
+            }
+
+            # - Right now both are "MODULE-FOO-VERSION-EXT" >
+            my $cpan_version = $cpan_pn;
+
+            # - Drop "-VERSION-EXT" from cpan_pn >
+            $cpan_pn =~
+              s/(?:-?)?(?:v?[\d\.]+[a-z]?)?\.(?:tar|tgz|zip|bz2|gz|tar\.gz)?$//;
+
+            if ( length( lc($cpan_version) ) >= length( lc($cpan_pn) ) ) {
+
+                # - Drop "MODULE-FOO-" from version >
+                if ( length( lc($cpan_version) ) == length( lc($cpan_pn) ) ) {
+                    $cpan_version = 0;
+                }
+                else {
+                    $cpan_version = substr(
+                        $cpan_version,
+                        length( lc($cpan_pn) ) + 1,
+                        length( lc($cpan_version) ) - length( lc($cpan_pn) ) - 1
+                    );
+                }
+                if ( defined $cpan_version ) {
+                    $cpan_version =~ s/\.(?:tar|tgz|zip|bz2|gz|tar\.gz)?$//;
+
+    # - Remove any leading/trailing stuff (like "v" in "v5.2.0") we don't want >
+                    $cpan_version =~ s/^[a-zA-Z]+//;
+                    $cpan_version =~ s/[a-zA-Z]+$//;
+
+                    # - Convert CPAN version >
+                    @tmp_v = split( /\./, $cpan_version );
+                    if ( $#tmp_v > 1 ) {
+                        if ($main::DEBUG) {
+                            print " converting version -> " . $cpan_version;
+                        }
+                        $cpan_version = $tmp_v[0] . ".";
+                        for ( 1 .. $#tmp_v ) { $cpan_version .= $tmp_v[$_]; }
+                        if ($main::DEBUG) { print " -> " . $cpan_version . "\n"; }
+                    }
+
+                    if ( $cpan_version eq "" ) { $cpan_version = 0; }
+
+                    $main::modules{'cpan'}{$cpan_pn} = $cpan_version;
+                    $main::modules{'cpan_lc'}{ lc($cpan_pn) } = $cpan_version;
+                }
+            }
+        }
+    }
+    return 0;
+}
 
 # Preloaded methods go here.
 
---
+++ lib/Gentoo/Config.pm
@@ -0,0 +1,160 @@
+package Gentoo::Config;
+
+use 5.008007;
+use strict;
+use warnings;
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+our @EXPORT = qw(getParamFromFile getFileContents getValue );
+
+
+our $VERSION = '0.01';
+
+sub new {
+     my $self  = shift;
+     my $class = ref($self) || $self;
+     return bless {}, $class;
+}
+
+# Description:
+# Returns the value of $param. Expects filecontents in $file.
+# $valueOfKey = getParamFromFile($filecontents,$key);
+# e.g.
+# $valueOfKey = getParamFromFile(getFileContents("/path/to.ebuild","IUSE","firstseen");
+sub getParamFromFile {
+    my $file  = shift;
+    my $param = shift;
+    my $mode  = shift;   # ("firstseen","lastseen") - default is "lastseen"
+    my $c     = 0;
+    my $d     = 0;
+    my @lines = ();
+    my @aTmp  = ();      # temp (a)rray
+    my $sTmp  = "";      # temp (s)calar
+    my $text  = "";      # complete text/file after being cleaned up and striped
+    my $value = "";      # value of $param
+    my $this  = "";
+
+    # - 1. split file in lines >
+    @lines = split( /\n/, $file );
+
+    # - 2 & 3 >
+    for ( $c = 0 ; $c <= $#lines ; $c++ ) {
+
+       # - 2. remove leading and trailing whitespaces and tabs from every line >
+        $lines[$c] =~ s/^[ |\t]+//;    # leading whitespaces and tabs
+        $lines[$c] =~ s/[ |\t]+$//;    # trailing whitespaces and tabs
+
+        # - 3. remove comments >
+        $lines[$c] =~ s/#(.*)//g;
+
+        if ( $lines[$c] =~ /^$param="(.*)"/ ) {
+
+            # single-line with quotationmarks >
+            $value = $1;
+
+            if ( $mode eq "firstseen" ) {
+
+                # - 6. clean up value >
+                $value =~ s/^[ |\t]+//;   # remove leading whitespaces and tabs
+                $value =~ s/[ |\t]+$//;   # remove trailing whitespaces and tabs
+                $value =~ s/\t/ /g;       # replace tabs with whitespaces
+                $value =~
+                  s/ {2,}/ /g;    # replace 1+ whitespaces with 1 whitespace
+                return $value;
+            }
+        }
+        elsif ( $lines[$c] =~ /^$param="(.*)/ ) {
+
+            # multi-line with quotationmarks >
+            $value = $1 . " ";
+            for ( $d = $c + 1 ; $d <= $#lines ; $d++ ) {
+
+                # - look for quotationmark >
+                if ( $lines[$d] =~ /(.*)"/ ) {
+
+                    # - found quotationmark; append contents and leave loop >
+                    $value .= $1;
+                    last;
+                }
+                else {
+
+                    # - no quotationmark found; append line contents to $value >
+                    $value .= $lines[$d] . " ";
+                }
+            }
+
+            if ( $mode eq "firstseen" ) {
+
+                # - 6. clean up value >
+                $value =~ s/^[ |\t]+//;   # remove leading whitespaces and tabs
+                $value =~ s/[ |\t]+$//;   # remove trailing whitespaces and tabs
+                $value =~ s/\t/ /g;       # replace tabs with whitespaces
+                $value =~
+                  s/ {2,}/ /g;    # replace 1+ whitespaces with 1 whitespace
+                return $value;
+            }
+        }
+        elsif ( $lines[$c] =~ /^$param=(.*)/ ) {
+
+            # - single-line without quotationmarks >
+            $value = $1;
+
+            if ( $mode eq "firstseen" ) {
+
+                # - 6. clean up value >
+                $value =~ s/^[ |\t]+//;   # remove leading whitespaces and tabs
+                $value =~ s/[ |\t]+$//;   # remove trailing whitespaces and tabs
+                $value =~ s/\t/ /g;       # replace tabs with whitespaces
+                $value =~
+                  s/ {2,}/ /g;    # replace 1+ whitespaces with 1 whitespace
+                return $value;
+            }
+        }
+    }
+
+    # - 6. clean up value >
+    $value =~ s/^[ |\t]+//;       # remove leading whitespaces and tabs
+    $value =~ s/[ |\t]+$//;       # remove trailing whitespaces and tabs
+    $value =~ s/\t/ /g;           # replace tabs with whitespaces
+    $value =~ s/ {2,}/ /g;        # replace 1+ whitespaces with 1 whitespace
+
+    return $value;
+}
+
+# Description:
+# Returnvalue is the content of the given file.
+# $filecontent = getFileContents($file);
+sub getFileContents {
+    my $content = "";
+
+    open( FH, "<" . $_[0] ) || die( "Cannot open file " . $_[0] );
+    while (<FH>) { $content .= $_; }
+    close(FH);
+    return $content;
+}
+
+sub getValue {
+	my $self = shift;
+	my $confVal = shift;
+	my $makeconf = getParamFromFile( getFileContents("/etc/make.conf"), "$confVal","lastseen" );
+	if (! $makeconf ) 
+	{
+		$makeconf = getParamFromFile( getFileContents("/etc/make.globals"), "$confVal", "lastseen" );
+	}
+	
+	if ( ! $makeconf )
+	{
+		return undef
+	}
+	if ( $makeconf =~ m/\b\s.*/ )
+	{
+		my @makeconf = map { split ' ' } $makeconf;
+		return \@makeconf;
+
+	}
+	return $makeconf;
+}
+
+1;
